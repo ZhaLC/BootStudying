4c71cd752267f0cb80905fb80878fdba6f03eccf
ZhaLiCun <t_zhalc@channelsoft.com>
2019-02-15 11:12:57 +0800

    整合Redis没看懂

---
+++ src/main/java/com/zlc/BootStudying/config/RedisConfig.java
@@ -0,0 +1,68 @@
+package com.zlc.BootStudying.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author : ZLC
+ * @create : 2019-02-14 16:09
+ * @desc : Redis缓存的配置类
+ **/
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport{
+
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator(){
+            @Override
+            public Object generate(Object target, Method method, Object... params){
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+
+        };
+    }
+
+    //设置Redis换存配置类
+    @SuppressWarnings("rawtypes")
+    @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate){
+        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
+        return rcm;
+
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
---
+++ src/main/java/com/zlc/BootStudying/po/User.java
@@ -0,0 +1,23 @@
+package com.zlc.BootStudying.po;
+
+/**
+ * @author : ZLC
+ * @create : 2019-02-14 17:13
+ * @desc :
+ **/
+public class User {
+
+    private String userName;
+    private String email;
+    private String telNum;
+    private String address;
+    private String aaa;
+
+    public User(String userName, String email, String telNum, String address, String aaa) {
+        this.userName = userName;
+        this.email = email;
+        this.telNum = telNum;
+        this.address = address;
+        this.aaa = aaa;
+    }
+}
