75a6ad3bf1f1bff476b7178fba7282a1f6723128
ZhaLiCun <t_zhalc@channelsoft.com>
2019-02-27 09:34:05 +0800

    kafka

---
+++ src/main/java/com/zlc/BootStudying/kafka/KafkaProducer.java
@@ -0,0 +1,33 @@
+package com.zlc.BootStudying.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import java.util.UUID;
+
+
+/**
+ * @author : ZLC
+ * @create : 2019-02-19 14:19
+ * @desc : kafka生产者
+ **/
+@Component
+@EnableScheduling
+public class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    @Scheduled(cron = "0 0/1 * * * * ?")
+    public void send(){
+        String message = UUID.randomUUID().toString();
+        ListenableFuture future = kafkaTemplate.send("app_log", message);
+        future.addCallback(o -> System.out.println("send-消息发送成功：" + message),
+                throwable -> System.out.println("消息发送失败：" + message));
+    }
+
+}
