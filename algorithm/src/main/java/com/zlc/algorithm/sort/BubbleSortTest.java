83b51d915fca6a853fbdcb08d7486447b041c16f
ZhaLiCun <zhalc@channelsoft.com>
2020-04-09 17:25:07 +0800

    鸡尾酒排序(sort4、sort5)

--- algorithm/src/main/java/com/zlc/algorithm/sort/BubbleSortTest.java
+++ algorithm/src/main/java/com/zlc/algorithm/sort/BubbleSortTest.java
@@ -84,24 +84,87 @@ public class BubbleSortTest {
     }
 
     /**
-     * 鸡尾酒排序(双向)
+     * 鸡尾酒排序(双向, 适用于大部分都是有序的情况, 也是O(n²))
+     * 不添加有序区 也可以添加有序区逻辑
+     * https://mp.weixin.qq.com/s/CoVZrvis6BnxBQgQrdc5kA
      * @param arr
      * @return
      */
     public static int[] sort4(int[] arr){
-
-        for(int i = 0; i < arr.length-1; i++){
-            boolean change = false;
-            for(int j = 0; j < arr.length-i-1; j++ ){
-                int temp = 0;
+        int temp = 0;
+        for(int i = 0; i < arr.length/2; i++){
+            boolean isSorted = true;
+            //奇数轮 从左到右比较和交换
+            for(int j = i; j < arr.length-1-i; j++ ){
                 if(arr[j] > arr[j+1]){
                     temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
-                    change = true;
+                    isSorted = false;
                 }
             }
-            if(!change){
+            if(isSorted){
+                break;
+            }
+            //偶数轮之前 从新标记为true
+            isSorted = true;
+            //偶数轮 从右向左比较和交换
+            for(int j = arr.length-1-i; j > i; j--){
+                if(arr[j] < arr[j-1]){
+                    temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    isSorted = false;
+                }
+            }
+            if(isSorted){
+                break;
+            }
+            System.out.format("第 %d 趟排序: \t", i);
+            print(arr);
+        }
+        return arr;
+    }
+
+    /**
+     * 鸡尾酒排序(添加了有序区 O(n²))
+     * @param arr
+     * @return
+     */
+    public static int[] sort5(int[] arr){
+        int temp = 0;
+        int lastRightExchangeIndex = 0;
+        int lastLeftExchangeIndex = 0;
+        int leftSortBorder = 0;
+        int rightSortBorder = arr.length-1;
+        for(int i = 0; i < arr.length/2; i++){
+            boolean isSorted = true;
+            for(int j = leftSortBorder; j < rightSortBorder; j++){
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    isSorted = false;
+                    lastRightExchangeIndex = j;
+                }
+            }
+            rightSortBorder = lastRightExchangeIndex;
+            if(isSorted){
+                break;
+            }
+
+            isSorted = true;
+            for(int j = rightSortBorder; j > leftSortBorder; j--){
+                if(arr[j] < arr[j-1]){
+                    temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    isSorted = false;
+                    lastLeftExchangeIndex = j;
+                }
+            }
+            leftSortBorder = lastLeftExchangeIndex;
+            if(isSorted){
                 break;
             }
             System.out.format("第 %d 趟排序: \t", i);
@@ -126,13 +189,18 @@ public class BubbleSortTest {
         int[] arr1 = {1,2,4,9,6,5,8,7,3};
         int[] arr2 = {5,8,6,3,9,2,1,7};
         int[] arr3 = {3,4,2,1,5,6,7,8};
+        int[] arr4 = {2,3,4,5,6,7,8,1};
+        int[] arr5 = {2,3,4,5,6,7,8,1};
+        System.out.println("=====1========");
         sort1(arr1);
-        System.out.println("=============");
+        System.out.println("=====2========");
         sort2(arr2);
-        System.out.println("=============");
+        System.out.println("=====3========");
         sort3(arr3);
-
-
+        System.out.println("=====4========");
+        sort4(arr4);
+        System.out.println("=====5========");
+        sort5(arr5);
     }
 
 }
