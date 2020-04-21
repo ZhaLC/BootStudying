b31c714019104331f5dab7d976e1190612a8b011
ZhaLiCun <zhalc@channelsoft.com>
2020-04-21 12:11:21 +0800

    单例模式改动补充

--- design-patterns/src/main/java/com/zlc/designpatterns/singleton/DCLSingleton.java
+++ design-patterns/src/main/java/com/zlc/designpatterns/singleton/DCLSingleton.java
@@ -3,7 +3,7 @@ package com.zlc.designpatterns.singleton;
 /**
  * @author : ZLC
  * @create : 2020-04-20 17:19
- * @desc : DCL(double check lock) 单例模式 推荐使用 线程安全 延迟加载 效率较高
+ * @desc : DCL(double check lock/loading?) 单例模式 推荐使用 线程安全 延迟加载 效率较高
  **/
 public class DCLSingleton {
 
@@ -16,6 +16,10 @@ public class DCLSingleton {
             synchronized (DCLSingleton.class){
                 if(singleton == null){
                     //这里不是原子的 如果上面不加volatile 可能会获取到半初始化对象(没有赋值)
+                    //对应三条CPU指令
+                    //1、为Singleton分配一块内存M
+                    //2、在内存M上对singleton进行初始化
+                    //3、将内存M地址赋值给singleton变量
                     singleton = new DCLSingleton();
                 }
             }
