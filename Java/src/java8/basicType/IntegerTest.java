c1ec716dd738c72f4c6a6d73c9dd49810abc40cf
ClearZero <zhalc@channelsoft.com>
2020-07-23 17:25:36 +0800

    int/Integer

--- Java/src/java8/basicType/IntegerTest.java
+++ Java/src/java8/basicType/IntegerTest.java
@@ -19,6 +19,9 @@ public class IntegerTest {
         Integer num2 = 400;
         System.out.println(num1 == num2); //false
         System.out.println(num1.equals(num2)); //true
+        //再看下面的int
+
+        System.out.println("====================");
 
 
         String str = "abc";
@@ -26,6 +29,22 @@ public class IntegerTest {
         System.out.println(str==str2); //true
         String str3 = new String("abc");
         String str4 = new String("abc");
-        System.out.print(str3==str4); //false
+        System.out.println(str3==str4); //false
+
+        System.out.println("===================");
+
+        int a = 100;
+        int b = 200;
+        Integer c = 100;
+        Integer d = 200;
+        Integer e = new Integer(200);
+        Integer f = new Integer(200);
+        System.out.println(a == c); //true
+        System.out.println(b == d); //true
+        System.out.println(d == e); //false
+        System.out.println(b == e); //true
+        System.out.println(e == b); //true
+        System.out.println(d.equals(b)); //true
+        System.out.println(e == f); //false
     }
 }
