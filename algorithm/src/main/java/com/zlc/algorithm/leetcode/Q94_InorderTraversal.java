2384ac68445ed41cfd4286f8f2ac614b5a8e7385
ClearZero <zhalc@channelsoft.com>
2020-09-15 10:14:06 +0800

    Q94_InorderTraversal 二叉树中序遍历(递归实现)

---
+++ algorithm/src/main/java/com/zlc/algorithm/leetcode/Q94_InorderTraversal.java
@@ -0,0 +1,37 @@
+package com.zlc.algorithm.leetcode;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.*;
+
+/**
+ * @desc :
+ **/
+public class Q94_InorderTraversal {
+
+    //递归
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> ans = new ArrayList<>();
+//        inOrder(ans, root);
+//        return ans;
+//    }
+//    private void inOrder(List<Integer> ans, TreeNode root) {
+//        if(root == null){
+//            return;
+//        }
+//        inOrder(ans, root.left);
+//        ans.add(root.val);
+//        inOrder(ans, root.right);
+//    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
